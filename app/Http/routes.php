d3c9829ad7c1b8b3241b0fefd53d4d899592022b
JavidSapand <javid.sapand@gmail.com>
2015-04-14 16:28:34 +0430

    uncompleted changes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,13 @@ Route::get('admin', function(){
 	$users = User::all();
 	return View::make('admin.home', compact('users'));
 });
+Route::post('ajax/edit', function(){
+	 if(Request::ajax()){
+        return "AJAX";
+    }
+    return "HTTP";
+	
+});
 
 // Route::get('booking','BookingController@index');
 // Route::get('booking/create','BookingController@create');
